25227b7bc78af30f162df55800335c87ddc59200
StruggleYang <1724555319@qq.com>
2023-04-20 20:17:59 +0800

    new case

--- demo_test.go
+++ demo_test.go
@@ -4,11 +4,9 @@ import "testing"
 
 func TestPrints(t *testing.T) {
 	prints(true)
-}
-
-func TestPrints2(t *testing.T) {
-	prints(true)
+	prints(false)
 }
 func TestPrints2_1(t *testing.T) {
 	prints2(true)
+	prints2(false)
 }
