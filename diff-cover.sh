94e791c4bb51cae9ee37e3ce6e0db4df23f2cd90
StruggleYang <1724555319@qq.com>
2023-04-21 10:33:54 +0800

    fix

--- diff-cover.sh
+++ diff-cover.sh
@@ -1,4 +1,5 @@
 # 执行所有单测,并将其转换为覆盖率文件
-gocov test ./... | gocov-html > out/coverage.html
-gocov test ./... | gocov-xml > out/coverage.xml
+gocov test ./... > out/coverage.json
+cat out/coverage.json| gocov-html > out/coverage.html
+cat out/coverage.json| gocov-xml > out/coverage.xml
 diff-cover --compare-branch baad4639  out/coverage.xml --html-report out/report.html
